142b203d31c4d03771cb32be419bf4a2da7dd2e6
kjonak <zgrywcio@gmail.com>
2022-03-16 22:13:16 +0100

    thrusters struct is an array

--- Src/IO/thrusters.c
+++ Src/IO/thrusters.c
@@ -1,7 +1,10 @@
 #include "thrusters.h"
 #include "time/time.h"
+#include <stdbool.h>
 
-#define T_INIT_VALUE 1500
+#define T_INIT_VALUE 1500.f
+#define T_MAX_VALUE 2000.f
+#define T_MIN_VALUE 1000.f
 #define THRUSTER_1  TIM1->CCR4
 #define THRUSTER_2  TIM3->CCR4
 #define THRUSTER_3  TIM1->CCR4
@@ -10,36 +13,37 @@
 #define THRUSTER_6  TIM3->CCR2
 #define THRUSTER_7  TIM1->CCR1
 #define THRUSTER_8  TIM3->CCR1
+
+bool allow_update = false;
 void setThruster(uint32_t thruster, uint32_t value)
 {
     thruster = value;
 }
-void resetThrusters_pwm_t(thrusters_pwm_t *out)
+void resetThrusters_pwm_t(uint16_t *out)
 {
-out->T1 = T_INIT_VALUE;
-out->T2 = T_INIT_VALUE;
-out->T3 = T_INIT_VALUE;
-out->T4 = T_INIT_VALUE;
-out->T5 = T_INIT_VALUE;
-out->T6 = T_INIT_VALUE;
-out->T7 = T_INIT_VALUE;
-out->T8 = T_INIT_VALUE;
+    for(uint8_t i =0; i<THRUSTERS_COUNT; i++)
+    {
+        out[i] = T_INIT_VALUE;
+    }
 }
 
-void setThrusters(thrusters_pwm_t *out)
+void setThrusters(uint16_t *out)
 {
-    THRUSTER_1 = (uint32_t)out->T1;
-    THRUSTER_2 = (uint32_t)out->T2;
-    THRUSTER_3 = (uint32_t)out->T3;
-    THRUSTER_4 = (uint32_t)out->T4;
-    THRUSTER_5 = (uint32_t)out->T5;
-    THRUSTER_6 = (uint32_t)out->T6;
-    THRUSTER_7 = (uint32_t)out->T7;
-    THRUSTER_8 = (uint32_t)out->T8;
+    if(!allow_update)
+        return;
+    THRUSTER_1 = (uint32_t)out[0];
+    THRUSTER_2 = (uint32_t)out[1];
+    THRUSTER_3 = (uint32_t)out[2];
+    THRUSTER_4 = (uint32_t)out[3];
+    THRUSTER_5 = (uint32_t)out[4];
+    THRUSTER_6 = (uint32_t)out[5];
+    THRUSTER_7 = (uint32_t)out[6];
+    THRUSTER_8 = (uint32_t)out[7];
 }
 
 void initThrusters()
 {
+    allow_update = true;
     TIM1->BDTR |= TIM_BDTR_MOE;
     TIM1->CR1 |= TIM_CR1_CEN;
     TIM3->CR1 |= TIM_CR1_CEN;
@@ -56,6 +60,7 @@ void initThrusters()
 
 void deinitThrusters()
 {
+    allow_update=false;
 THRUSTER_1 = T_INIT_VALUE;
 THRUSTER_2 = T_INIT_VALUE;
 THRUSTER_3 = T_INIT_VALUE;
@@ -67,4 +72,24 @@ THRUSTER_8 = T_INIT_VALUE;
 TIM1->BDTR &= ~TIM_BDTR_MOE;
 TIM1->CR1 &= ~TIM_CR1_CEN;
 TIM3->CR1 &= ~TIM_CR1_CEN;
+}
+
+void THRUSTER_Enable()
+{
+    allow_update = true;
+}
+void THRUSTERS_Disable()
+{
+allow_update=false;
+THRUSTER_1 = T_INIT_VALUE;
+THRUSTER_2 = T_INIT_VALUE;
+THRUSTER_3 = T_INIT_VALUE;
+THRUSTER_4 = T_INIT_VALUE;
+THRUSTER_5 = T_INIT_VALUE;
+THRUSTER_6 = T_INIT_VALUE;
+THRUSTER_7 = T_INIT_VALUE;
+THRUSTER_8 = T_INIT_VALUE;
+}
+float THRUSTERS_map(float x, float in_min, float in_max) {
+  return (x - in_min) * (T_MAX_VALUE - T_MIN_VALUE) / (in_max - in_min) + T_MIN_VALUE;
 }
--- Src/IO/thrusters.h
+++ Src/IO/thrusters.h
@@ -1,22 +1,16 @@
 #pragma once
 #include "stm32f4xx.h"
+#include <stdint.h>
+#define THRUSTERS_COUNT 8
 
 
 
 
-typedef struct{
-    uint16_t T1;
-    uint16_t T2;
-    uint16_t T3;
-    uint16_t T4;
-    uint16_t T5;
-    uint16_t T6;
-    uint16_t T7;
-    uint16_t T8;
-}thrusters_pwm_t;
-
-void setThrusters(thrusters_pwm_t* out);
+void setThrusters(uint16_t* out);
 void setThruster(uint32_t thruster, uint32_t value);
 void initThrusters();
 void deinitThrusters();
-void resetThrusters_pwm_t(thrusters_pwm_t *out);
+void resetThrusters_pwm_t(uint16_t *out);
+void THRUSTERS_Disable();
+void THRUSTER_Enable();
+float THRUSTERS_map(float x, float in_min, float in_max);
